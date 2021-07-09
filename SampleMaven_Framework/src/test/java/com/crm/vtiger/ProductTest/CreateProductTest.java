6866a8040c7f0895fdb16fc99d9881650afb15d7
SOUMYASANTA SAHOO <SOUMYASANTA SAHOO@192.168.1.38>
2021-07-09 16:25:14 +0530

    commiting

---
+++ SampleMaven_Framework/src/test/java/com/crm/vtiger/ProductTest/CreateProductTest.java
@@ -0,0 +1,19 @@
+package com.crm.vtiger.ProductTest;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+	
+	@Test
+	public void createProductWithNameTest()
+	{
+		System.out.println("execute createProduct with name");
+	}
+	
+	@Test
+	public void createProductWithId()
+	{
+		System.out.println("execute createProduct with id");
+	}
+
+}
---
+++ SampleMaven_Framework/src/test/java/com/crm/vtiger/ProductTest/SearchProductTest.java
@@ -0,0 +1,19 @@
+package com.crm.vtiger.ProductTest;
+
+import org.testng.annotations.Test;
+
+public class SearchProductTest {
+
+	@Test
+	public void searchProductWithNameTest()
+	{
+		System.out.println("execute searchProduct with name");
+	}
+	
+	@Test
+	public void searchProductWithId()
+	{
+		System.out.println("execute searchProduct with id");
+	}
+	
+}
