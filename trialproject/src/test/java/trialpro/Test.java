61055fb32f56162e16d4de052b214237e211145f
god <god@DESKTOP-2A6M2A1>
2021-07-17 00:28:00 -0700

    first time commit

--- trialproject/src/test/java/trialpro/Test.java
+++ trialproject/src/test/java/trialpro/Test.java
@@ -1,9 +1,8 @@
 package trialpro;
 
-public class Test1 {
-
+public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	 System.out.println("hhhh");
-	}
+}
 }
